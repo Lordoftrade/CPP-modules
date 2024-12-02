ac07003c8634407c84fc2adf5ce00e75cdfa5df8
lordoftrade <lordoftrade@rambler.ru>
2024-12-02 21:58:40 +0400

    update CPP02/ex00

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -5,21 +5,23 @@ Fixed::Fixed() : value(0)
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::~Fixed()
-{
-	std::cout << "Destructor called" << std::endl;
-}
-Fixed::Fixed(const Fixed& another) : value(another.value)
+Fixed::Fixed(const Fixed& another)
 {
 	std::cout << "Copy constructor called" << std::endl;
+	*this = another;
 }
 
 Fixed& Fixed::operator=(const Fixed& another)
 {
+	std::cout << "Copy assignment operator called" << std::endl;
 	if (this == &another) {return *this;}
-
-	value = another.value;
+	value = another.getRawBits();
 	return *this;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
 }
 
 int Fixed::getRawBits(void) const
